02969f79fab972a96ffcbffd585f01dc8a24458a
Vincent Kranendonk <kranendonk.vincent@gmail.com>
2016-07-11 15:16:32 +0200

    Height preview full height (50% of screen)

--- src/reducers/code_reducer.js
+++ src/reducers/code_reducer.js
@@ -4,7 +4,7 @@ import {
 
 let defaults = {
         html: '<div class="block"><h1>Hello, World!</h1></div>',
-        css: '.block {\n\twidth: 300px;\n\theight:70px;\n\tbackground-color: pink;\n}\nh1 {\n\tpadding: 10px;\n}',
+        css: 'html, body { margin:0; padding:0; background-color: lightcoral; color: #fff; font-family: "Helvetica"; height:100%}\n\ \n\.block {\n\ text-align:center;\n\ width:100%;\n\ align-items: center;\n\ display:flex;\n\ height:100%;}\n\ \n\h1 {\n\ padding: 0;\n\ width:100%;\n\}',
         javascript: 'var component = {\n\tname: "react-codemirror",\n\tauthor: "Jed Watson",\n\trepo: "https://github.com/JedWatson/react-codemirror"\n};'
 };
 
