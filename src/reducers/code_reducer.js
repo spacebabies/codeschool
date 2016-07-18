c8c1a10d1ee3797acefcb601170cad22075b9a7f
Vincent Kranendonk <kranendonk.vincent@gmail.com>
2016-07-18 23:48:43 +0200

    Space Babies theme

--- src/reducers/code_reducer.js
+++ src/reducers/code_reducer.js
@@ -3,8 +3,8 @@ import {
 } from '../actions/types';
 
 let defaults = {
-        html: '<div class="block"><h1>Hello, World!</h1></div>',
-        css: 'html, body { margin:0; padding:0; background-color: lightcoral; color: #fff; font-family: "Helvetica"; height:100%}\n\ \n\.block {\n\ text-align:center;\n\ width:100%;\n\ align-items: center;\n\ display:flex;\n\ height:100%;}\n\ \n\h1 {\n\ padding: 0;\n\ width:100%;\n\}',
+        html: '<div class="block"><h1>Space Babies</h1></div>',
+        css: 'html, body { margin:0; padding:0; background-color: black; color: #fff; font-family: "Helvetica"; height:100%}\n\ \n\.block {\n\ text-align:center;\n\ width:100%;\n\ align-items: center;\n\ display:flex;\n\ height:100%;}\n\ \n\h1 {\n\ padding: 0;\n\ width:100%;\n\ letter-spacing: 30px;\n\ text-transform: uppercase;}',
         javascript: 'var component = {\n\tname: "react-codemirror",\n\tauthor: "Jed Watson",\n\trepo: "https://github.com/JedWatson/react-codemirror"\n};'
 };
 
