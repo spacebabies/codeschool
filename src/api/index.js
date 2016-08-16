8c9063e227ff8a47a626f11888c2086bce55a0eb
Melanie Keatley <melly.makes@gmail.com>
2016-08-16 14:01:23 +0200

    Updates the prefilled code

--- src/api/index.js
+++ src/api/index.js
@@ -126,7 +126,7 @@ class Api {
                 // Temporary data for development
                 let JS_code = "var%20i%20%3D%20%22Test%20JS%20editor%22%3B%0A%0Afunction%20test()%20%7B%0A%20%20console.log(i)%3B%0A%7D%0A%0Atest()%3B";
                 let HTML_code = "%3Cdiv%20class%3D%22block%22%3E%3Ch1%3EWelkom%20bij%20Code%20School%3C%2Fh1%3E%3C%2Fdiv%3E";
-                let CSS_code = encodeURIComponent('html, body {\n margin:0;\n padding:0;\n background-color: #a8d8b6;\n color: #000;\n font-family: "Helvetica";\n height:100%}\n\ \n\.block {\n\ text-align:center;\n\ width:100%;\n\ align-items: center;\n\ display:flex;\n\ height:100%;}\n\ \n\h1 {\n\ padding: 0;\n\ width:100%;\n\ letter-spacing: 10px;}');
+                let CSS_code = "html%2C%20body%20%7B%0A%20margin%3A0%3B%0A%20padding%3A0%3B%0A%20background-color%3A%20%23a8d8b6%3B%0A%20color%3A%20%23000%3B%0A%20font-family%3A%20%22Helvetica%22%3B%0A%20height%3A100%25%7D%0A%20%0A.block%20%7B%0A%20text-align%3Acenter%3B%0A%20width%3A100%25%3B%0A%20align-items%3A%20center%3B%0A%20display%3Aflex%3B%0A%20height%3A100%25%3B%7D%0A%20%0Ah1%20%7B%0A%20padding%3A%200%3B%0A%20width%3A100%25%3B%0A%20letter-spacing%3A%2010px%3B%7D";
 
                 dispatch({
                     type: JS,
