3e6791d80aa3f46cde99a05f11850e874f95d43d
Melanie Keatley <melly.makes@gmail.com>
2016-09-26 14:03:35 +0200

    Message bar is updated after a student successfully fullfils the aassignment

--- src/components/assignment-nav.js
+++ src/components/assignment-nav.js
@@ -9,22 +9,29 @@ import '../../style/AssignmentNav.scss';
 class AssignmentNav extends Component {
 
   render() {
-
     if(this.props.correct) {
+      if (document.getElementById("BottomBar") !== null) {
+        document.getElementById("BottomBar").style.visibility = "visible";
+      };
       return (
-        <div className="AssignmentNav">
-          <div className="assignment_nav">
-            <h2 className="animated pulse">Klik op verder om door te gaan</h2>
-              <button onClick={this.nextAssignment.bind(this)}>Verder</button>
+        <div id="BottomBar">
+          <div className="AssignmentNav">
+            <div className="assignment_nav">
+              <h2 className="animated pulse">Heel goed! Klik op verder</h2>
+                <button onClick={this.nextAssignment.bind(this)}>Verder</button>
+            </div>
           </div>
         </div>
       );
+
     }
     return null;
   }
 
   nextAssignment() {
     this.props.getApiData();
+    document.getElementById("BottomBar").style.visibility = "hidden";
+    return false;
   }
 
 }
